047a6520bf5738295cabe779c5e435a58a4d5c33
tadahayamiz <tadahaya@gmail.com>
2023-09-14 01:30:56 +0900

    debug

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
+# modify entry_points to use command line 
+# {COMMAND NAME}={module path}:{function in the module}
 setup(
-    name='nn_temp',
-    packages=find_packages(),
-    version='0.0.1',
-    description='template for a neural network package',
-    author='tadahaya',
-    license='MIT',
+    name="nn_temp",
+    version="0.0.1",
+    description="a CLI package for handling NN",
+    author="tadahaya",
     packages=find_packages(),
     install_requires=install_requirements,
     include_package_data=True,
     entry_points={
         "console_scripts": [
-            "nn_temp.dev=230913-01:main",
+            "nn_temp.dev=nn_temp.230913-01:main",
         ]
     },
     classifiers=[
